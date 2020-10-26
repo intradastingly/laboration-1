a3f9b8364b758cf3cc974a22303e259cf34605bc
Asaf Läckgren <asaf_lackgren@hotmail.com>
2020-10-26 14:04:09 +0100

    Added main js game logic for first question

--- script.js
+++ script.js
@@ -5,34 +5,45 @@ function onUserInput(){
     return choice;
 }
 
+//figure out how to reuse userchoice button for different questions
 function userChoice(){
-   let number = onUserInput();
-   if (number == 1){
-        document.getElementById("text").innerHTML = "you picked a piece of paper";
-        setTimeout(choice1, 2000);
-   } else if (number == 2){
-        document.getElementById("text").innerHTML = "you picked the business card";
-        setTimeout(choice2, 2000);
-   } else if (number == 3){
-        document.getElementById("text").innerHTML = "you picked the mobile phone";
-        setTimeout(choice3, 2000);
+   let choice = onUserInput();
+   if (choice == "Piece of paper"){
+        document.getElementById("text").innerHTML = "You picked a piece of paper";
+        setTimeout(pieceOfPaper, 2000);
+   } else if (choice == "Business card"){
+        document.getElementById("text").innerHTML = "You picked the business card";
+        setTimeout(businessCard, 2000);
+   } else if (choice == "Mobile phone"){
+        document.getElementById("text").innerHTML = "You picked the mobile phone";
+        setTimeout(mobilePhone, 2000);
    } else {
-        document.getElementById("text").innerHTML = "please pick 1, 2 or 3";
+        document.getElementById("text").innerHTML = "Please select an option";
    }
 }
 
-function choice1(){
-    document.getElementById("text").innerHTML = "The Piece of paper has an address written on it, after walking through the city for several hours asking strangers for directions. You arrive at the address. There is a man leaning against the wall next to the door, do you...";
-    console.log("you picked a piece of paper")
+
+//add all user choices to if statement
+function pieceOfPaper(){
+    document.getElementById("text").innerHTML = "The Piece of paper has an address written on it, after several hours asking strangers for directions. You arrive at the address. There is a man leaning against the wall next to the door, do you...";
+    document.getElementById("option1").innerHTML = "Talk to the man";
+    document.getElementById("option2").innerHTML = "Knock on the door";
+    document.getElementById("option3").innerHTML = "Leave";
 }
 
-function choice2(){
-    document.getElementById("text").innerHTML = "you picked a piece of paper";
-    console.log("business card")
-
+function businessCard(){
+    document.getElementById("text").innerHTML = "The Business Card has an address written on it, after several hours asking strangers for directions. You arrive at the address. It's an unimaginably tall skycraper with armed gaurds standing outside, do you...";
+    document.getElementById("option1").innerHTML = "Attack the Gaurds";
+    document.getElementById("option2").innerHTML = "Try to enter";
+    document.getElementById("option2").innerHTML = "Leave";
 }
 
-function choice3(){
-    document.getElementById("text").innerHTML = "you picked a piece of paper";
-    console.log("piece of paper")
+function mobilePhone(){
+    document.getElementById("text").innerHTML = "After wandering around the city for a few hours, asking people if they have a charger(it's an ancient 2020 model phone). You find an antique shop ";
+    document.getElementById("option1").innerHTML = "Talk to the man";
+    document.getElementById("option2").innerHTML = "Knock on the door";
+    document.getElementById("option2").innerHTML = "Leave";
 }
+
+//add end page with play again options. 
+
